e55691da6e2e8594fd1980b5ec68c68a86b4dafb
osbornjr <osbornjr@rose-hulman.edu>
2017-09-06 12:35:59 -0400

    Everything is Finished. Had trouble figuring out the end bound for m9, but I figured it out with some help.

--- src/m7_why_parameters_are_powerful.py
+++ src/m7_why_parameters_are_powerful.py
@@ -64,7 +64,7 @@ def draw_circles():
     window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
-# TODO: 2.
+# Done: 2.
 #   First, RUN this program.  You will see that draw_circles draws
 #   concentric circles whose radii vary by 10.
 #
@@ -114,7 +114,7 @@ def test_better_draw_circles():
     better_draw_circles(3)
 
 # ----------------------------------------------------------------------
-# TODO: 3.
+# Done: 3.
 #   In the previous exercise, you made a MORE POWERFUL version
 #   of draw_circles by introducing a PARAMETER for the amount by
 #   which the radii of the concentric circles increase.
